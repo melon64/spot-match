79a925d38b2cd5382b92a2bc1b88f2cc9b21d01c
nathn101 <nathan.ch.chu@gmail.com>
2023-01-07 13:44:14 -0500

    updated SigninForm

--- src/SigninForm.js
+++ src/SigninForm.js
@@ -5,10 +5,10 @@ const SignIn = () => {
                 <h2>Login</h2>
 
                 <label htmlFor="email"><b>Email</b></label>
-                <input htmltype="text" htmlplaceholder="Enter Email" htmlname="email" htmlrequired/>
+                <input htmltype="text" htmlplaceholder="Enter Email" htmlname="email" required/>
 
                 <label htmlFor="psw"><b>Password</b></label>
-                <input htmltype="password" htmlplaceholder="Enter Password" htmlname="psw" htmlrequired/>
+                <input htmltype="password" htmlplaceholder="Enter Password" htmlname="psw" required/>
 
                 <button htmltype="submit" className="btn">Login</button>
             </form>
